2d2c187aaee32e07ecdd6c9e89e7d313ee271a26
wangzhengquan <wangzhengquan85@126.com>
2024-04-29 13:35:18 +0800

    update

--- auth.config.ts
+++ auth.config.ts
@@ -6,13 +6,14 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // console.log('nextUrl', nextUrl);
-      // console.log('nextUrl.searchParams', nextUrl.searchParams.toString());
+      console.log('nextUrl', nextUrl);
+      console.log('nextUrl.searchParams', nextUrl.searchParams.toString());
       let callbackUrl = nextUrl.searchParams.get('callbackUrl') || '/dashboard';
-      // console.log('callbackUrl', callbackUrl);
+      console.log('callbackUrl', callbackUrl);
       if (callbackUrl === nextUrl.origin+'/' ) {
         callbackUrl = '/dashboard';
       }
+      console.log('callbackUrl', callbackUrl);
       const isLoggedIn = !!auth?.user;
       const isOnLogin = nextUrl.pathname.startsWith('/login');
       if (isLoggedIn) {
