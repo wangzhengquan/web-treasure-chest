19f248c06cf6b94107fca68598cfb9754f851242
wangzhengquan <wangzhengquan85@126.com>
2024-05-27 16:33:32 +0800

    disable delete btn

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -29,8 +29,8 @@ export function UpdateInvoice({ id }: { id: string }) {
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <form action={deleteInvoiceWithId}>
-      
+    // <form action={deleteInvoiceWithId}>
+    <form> 
       <button type="submit" className="rounded-md border p-2 hover:bg-accent-200">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
